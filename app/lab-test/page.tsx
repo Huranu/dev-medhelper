ea6df292481c08fec30f61f45bbbc69bf1c3f14f
khulantuwshinbayar <khulantuwshinbayar@gmail.com>
2025-05-10 15:13:30 +0800

    lab test

--- app/lab-test/page.tsx
+++ app/lab-test/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import React from "react";
-import Input from "./components/input";
+// import Input from "./components/input";
+import { Input } from "@/components/ui/input"
 import Button from "./components/button";
 import Image from "next/image";
+import { FileIcon } from "lucide-react";
 
 const LabTestsScreening: React.FC = () => {
   const [message, setMessage] = React.useState<string>("");
   const [file, setFile] = React.useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = React.useState<string | null>(null)
 
   const handleSend = () => {
     if (message || file) {
@@ -20,23 +23,51 @@ const LabTestsScreening: React.FC = () => {
     }
   };
 
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0] || null
+    setFile(selectedFile)
+
+    if (selectedFile && selectedFile.type.startsWith("image/")) {
+      const objectUrl = URL.createObjectURL(selectedFile)
+      setPreviewUrl(objectUrl)
+    } else {
+      setPreviewUrl(null)
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
       <div className="text-center mb-8">
-        <Image src="/public/testResult.png" alt="Lab Icon" className="mx-auto mb-4" width={60} height={32}/>
+        <Image src="/testResult.png" alt="Lab Icon" className="mx-auto mb-4" width={200} height={200}/>
         <h1 className="text-2xl font-semibold">Лабораторийн болон эрт илрүүлэх үзлэгийн шинжилгээнүүд</h1>
         <p className="text-gray-600">
           Шинжилгээнүүдийн үр дүнгээ харж, бодит зөвлөгөө аван,
           биомаркерийн динамикаа хянаарай
         </p>
       </div>
-
       <Input
-        message={message}
-        file={file}
-        setMessage={setMessage}
-        setFile={setFile}
+        className="w-1/2 h-12 rounded-full px-4 py-2"
+        id="lab-test"
+        type="file"
+        onChange={handleFileChange}
       />
+
+      {file && (
+        <div className="flex flex-col items-center space-y-2">
+          {previewUrl ? (
+            <img
+              src={previewUrl}
+              alt="Preview"
+              className="w-32 h-20 object-cover rounded-md border"
+            />
+          ) : (
+            <div className="flex items-center space-x-2 text-zinc-500">
+              <FileIcon className="w-6 h-6" />
+              <span>{file.name}</span>
+            </div>
+          )}
+        </div>
+      )}
       <Button
         variant="submit"
         className="mt-5 w-36"
