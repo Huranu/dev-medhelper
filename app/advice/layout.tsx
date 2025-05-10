1d75b7053aa7136f741fc3b49ac0e63a533bd40d
Davakashi <daavka.desu@gmail.com>
2025-05-10 23:02:40 +0800

    small fixed

--- app/advice/layout.tsx
+++ app/advice/layout.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next'
+export const metadata: Metadata = {
+    title: 'Шинж тэмдэгийн үнэлгээ',
+    description: 'Start symptom assessment',
+}
+
 export default function AdviceLayout({
     children,
 }: {
