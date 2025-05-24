4bb1a073fa6906a70154d7377d8e8d7c51b07f96
Yesunkhast <esonkhastdorjpurev@gmail.com>
2025-05-25 04:32:32 +0800

    ah nar uuj neg negeere garj irj ug helev

--- app/lab-test/page.tsx
+++ app/lab-test/page.tsx
@@ -1,13 +1,14 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 
 import LabTestUpload from "./components/upload";
 import Forms from "./components/forms";
 import SpecimenSelection from "./components/specimen";
 import TypeSelection from "./components/types";
 
-import { Button } from "@/components/ui/button";
+import Button from "./components/button";
 import  {defineStepper } from '@stepperize/react';
+import Link from "next/link";
 
 const { useStepper, Scoped, steps } = defineStepper(
   { id: 'first', title: 'Ерөнхий мэдээлэл' },
@@ -19,7 +20,7 @@ const { useStepper, Scoped, steps } = defineStepper(
 
 const LabTestsScreening: React.FC = () => {
   return (
-    <div className=" flex flex-col gap-4 p-4 my-4 rounded-md w-full">
+    <div className=" flex flex-col gap-4 p-4 my-auto rounded-md w-full h-full">
         
     <Scoped>
       <ProgressBar />
@@ -34,9 +35,9 @@ export default LabTestsScreening;
 
 const Steps = () => {
   const stepper = useStepper();
-  const currentStep = stepper.current;
+  // const currentStep = stepper.current;
 
-  const isLastStep = stepper.isLast;
+  // const isLastStep = stepper.isLast;
   
   return (
     <div className="">
@@ -57,17 +58,35 @@ const Actions = () => {
 
   return !stepper.isLast ? (
     <div className="flex items-center gap-2 mx-auto">
-      <Button onClick={stepper.prev} disabled={stepper.isFirst}>
-        Өмнөх
-      </Button>
 
-      <Button onClick={stepper.next}>
+      {stepper.isFirst ? <Link href="/">
+        <Button
+        // disabled={stepper.isFirst} 
+        // handler={stepper.prev}
+        className="cursor-pointer border-purple-700 bg-gradient-to-br from-blue-500 to-purple-500 bg-clip-text text-transparent w-35 h-15"
+        size="large">
+          Буцах
+        </Button>
+      </Link> : <Button
+      disabled={stepper.isFirst} 
+      handler={stepper.prev}
+      className="cursor-pointer border-purple-700 bg-gradient-to-br from-blue-500 to-purple-500 bg-clip-text text-transparent w-35 h-15"
+      size="large">
+        Өмнөх
+      </Button>}
+
+
+      <Button  
+      handler={stepper.next}
+      className="w-35 h-15"
+      size="large"
+      >
         Дараах
       </Button>
     </div>
   ) : (
     <div className="flex items-center gap-2">
-      <Button onClick={stepper.reset}>Дахин эхлэх</Button>
+      <Button handler={stepper.reset}>Дахин эхлэх</Button>
     </div>
   );
 }
@@ -77,7 +96,7 @@ const ProgressBar: React.FC = () => {
   const currentIndex = steps.findIndex((s) => s.id === stepper.current.id);
 
   return (
-    <div className="flex flex-row items-center justify-center mr-10 mt-4 gap-4">
+    <div className="flex flex-row items-center justify-center mx-auto">
       {steps.slice(0, -1).map((step, i) => (
         <div key={step.id} className="flex flex-row items-center">
           {/* <span className="mb-1 text-xs font-medium text-gray-700">{step.title}</span> */}
@@ -94,7 +113,7 @@ const ProgressBar: React.FC = () => {
           </div>
           {i < steps.length - 2 && (
             <div
-              className={`w-10 h-1 my-1 transition-all duration-500 ease-in-out ${
+              className={`w-12 sm:w-30 h-0.5 transition-all duration-500 ease-in-out ${
                 currentIndex > i ? "bg-blue-500" : "bg-gray-300"
               }`}
             />
