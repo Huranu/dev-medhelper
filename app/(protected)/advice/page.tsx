db15d3d8db9555a9d715c6791fbd56b1f67d4a7b
Saihna <B231940056@must.edu.mn>
2025-05-25 22:10:24 +0800

    advice update

--- app/(protected)/advice/page.tsx
+++ app/(protected)/advice/page.tsx
@@ -4,10 +4,7 @@ import { useState } from 'react'
 import PrivateInfoForm from './_components/private-info-form'
 import SymptomsForm from './_components/Symptoms'
 import Response from './_components/response'
-import { motion } from 'framer-motion'
-import Link from 'next/link'
-import Image from 'next/image'
-import { ChevronLeft } from 'lucide-react'
+
 
 export default function WizardPage() {
   const [step, setStep] = useState(1)
@@ -26,33 +23,19 @@ export default function WizardPage() {
   }
 
   return (
-    <div className="relative min-h-screen bg-gradient-to-br from-blue-50 via-white to-blue-300 px-6 py-6 overflow-hidden">
-      <motion.header
-        className="w-full flex items-center px-6 py-4 bg-white shadow-md rounded-xl"
-        initial={{ y: -20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-      >
-        <div className="flex text-2xl font-extrabold text-blue-700 gap-6">
-          MedHelper
-          <Image width={30} height={15} src="/logo.jpg" alt="logo" />
-          <Link href="/">
-            <ChevronLeft className="cursor-pointer pt-1" height={33} width={33} />
-          </Link>
-        </div>
-      </motion.header>
-
+    <div className="h-screen bg-gradient-to-br from-blue-50 via-white to-blue-300 overflow-hidden">
+      
       {/* Main content */}
-      <div className="flex flex-row justify-center items-center w-full h-full mt-50 px-4">
+      <div className="flex flex-row justify-center items-center w-full h-full px-4">
         {/* Progress Bar */}
         <div className="flex flex-col items-center mr-10">
           {[1, 2, 3].map((s, i) => (
             <div key={s} className="flex flex-col items-center">
               <div
                 className={`w-15 h-15 rounded-full flex items-center justify-center text-2xl font-semibold border transition-colors duration-500 ${step === s
-                  ? 'bg-blue-600 text-white border-blue-600'
+                  ? 'bg-[#39ae9f] text-white'
                   : step > s
-                    ? 'bg-blue-500 text-white border-blue-500'
+                    ? 'bg-[#39ae9f] text-white'
                     : 'bg-white text-gray-500 border-gray-300'
                   }`}
               >
@@ -60,7 +43,7 @@ export default function WizardPage() {
               </div>
               {i < totalSteps - 1 && (
                 <div
-                  className={`w-px h-50 transition-all duration-500 ease-in-out ${step > s ? 'bg-blue-500' : 'bg-gray-300'
+                  className={`w-px h-50 transition-all duration-500 ease-in-out ${step > s ? 'bg-[#39ae9f]' : 'bg-gray-300'
                     }`}
                 />
               )}
